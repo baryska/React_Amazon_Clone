d430a4824a93ecb466b7ef1871b354647486f90b
baryska <55710842+baryska@users.noreply.github.com>
2020-09-11 17:54:56 +0200

    add non-interactive header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPx2Md-raCEG5SfUAobPSQUoqQkhxQ3g4",
+  authDomain: "clone-b6fcc.firebaseapp.com",
+  databaseURL: "https://clone-b6fcc.firebaseio.com",
+  projectId: "clone-b6fcc",
+  storageBucket: "clone-b6fcc.appspot.com",
+  messagingSenderId: "848403731016",
+  appId: "1:848403731016:web:8d6d707c5830b9401ebe7d",
+  measurementId: "G-7G2505TZVS"
+};
