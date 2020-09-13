39130aba1e100f20e39edc9a2e2802905507cbd0
baryska <55710842+baryska@users.noreply.github.com>
2020-09-14 00:14:24 +0200

    add register functionality with Firebase

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import "./Login.css"
 import { Link } from "react-router-dom"
+import { auth } from "./firebase"
 
 function Login() {
   const [email, setEmail] = useState('')
@@ -10,7 +11,12 @@ function Login() {
   }
   const register = e => {
     e.preventDefault()
-  
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((auth) => {
+          console.log(auth);
+        })
+        .catch(error => alert(error.message))
   }
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAPx2Md-raCEG5SfUAobPSQUoqQkhxQ3g4",
   authDomain: "clone-b6fcc.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:848403731016:web:8d6d707c5830b9401ebe7d",
   measurementId: "G-7G2505TZVS"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
